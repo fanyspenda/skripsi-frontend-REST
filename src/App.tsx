0968e30be5e92661cf06c051b4bc8fde7331b3a8
Fany Ervansyah <fanyervansyah.9c@gmail.com>
2020-02-14 10:32:09 +0700

    feat: add menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Menu, Dropdown } from "semantic-ui-react";
+import { Router, Link, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import AddAlumni from "./pages/addAlumni";
 
 const App = () => {
+  const history = createBrowserHistory();
+  const [activeMenu, setActiveMenu] = useState("A");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <Menu inverted color="blue" attached>
+        <Menu.Item header>Alumni Tracker</Menu.Item>
+        <Dropdown item text="Alumni">
+          <Dropdown.Menu>
+            <Dropdown.Item as={Link} to="/addAlumni">
+              Tambah Alumni
+            </Dropdown.Item>
+            <Dropdown.Item>Lihat Daftar Alumni</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu>
+      <Route exact path="/addAlumni" component={AddAlumni} />
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/addAlumni/index.tsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const AddAlumni = () => {
+  return <div>hahahaha</div>;
+};
+
+export default AddAlumni;
